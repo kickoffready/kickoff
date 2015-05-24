a2d96990a200b718449fb49f1fa055bd59dcea86
seeliang <me@sihuiliang.com>
2015-05-24 10:35:24 +1000

    initial

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var compass = require('gulp-compass'),
+  	path = require('path'),
+  	minifyCss = require('gulp-minify-css'),
+  	gulp = require('gulp');
+ 
+gulp.task('compass', function() {
+  gulp.src('./src/*.scss')
+    .pipe(compass({
+      project: path.join(__dirname, 'assets'),
+      css: 'css',
+      sass: 'scss'
+    }))
+    .pipe(gulp.dest('app/assets/temp'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/scss/*.scss', ['compass']);
+});
+
+gulp.task('min-css', function(){
+  return gulp.src('assets/css/main.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css/build'));	
+}); 
+
+gulp.task('default', ['compass', 'watch']);
