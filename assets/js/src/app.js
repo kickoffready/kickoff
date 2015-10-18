307f67ffd911b9019997cb6881b6a6c11e065d2e
seeliang <me@sihuiliang.com>
2015-10-18 16:54:58 +1100

    lint update

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -1,15 +1,15 @@
-var React = require('react'),
-  Demo = React.createClass({
-    render:function(){
-          console.log('this');
-      return(
-        <section className='react-demo'>
-          <h1> here is my react demo</h1>
-          <h2> something new</h2>
-        </section>
-      )
-    }
-  });
+var React = require('react');
+var Demo = React.createClass({
+  displayName: 'Demo',
+  render: function(){
+    return (
+      <section className={'react-demo'}>
+        <h1> {'here is my react demo'}</h1>
+        <h2> {'something new'}</h2>
+      </section>
+    );
+  }
+});
 
 React.render(
   <Demo/>,
