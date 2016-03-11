916becbf37dca7e385bd68d528e5a36fe137f4e6
seeliang <me@sihuiliang.com>
2016-03-11 12:59:34 +1100

    remove layout

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,8 @@ var gulp = require('gulp'),
     importCss = require('gulp-import-css'),
     minifyCss = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
-    clean = require('gulp-clean'),
     rename = require('gulp-rename'),
     bulkSass = require('gulp-sass-bulk-import');
-
-// make layout loads after structure
-gulp.task('rn-layout',function(){
-  return gulp.src('assets/scss/**/*layout.scss')
-    .pipe(rename(function(opt){
-      opt.basename = opt.basename.replace('-layout', '-xyz');
-      return opt;
-    }))
-    .pipe(gulp.dest('assets/scss'));
-});
-
-gulp.task('rm-layout',function(){
-  gulp.src('assets/scss/**/*layout.scss')
-  .pipe(clean());
-});
-
-gulp.task('rn-xyz',function(){
-  return gulp.src('assets/scss/**/*xyz.scss')
-    .pipe(rename(function(opt){
-      opt.basename = opt.basename.replace('-xyz', '-layout');
-      return opt;
-    }))
-    .pipe(gulp.dest('assets/scss'));
-});
-
-gulp.task('rm-xyz',function(){
-  gulp.src('assets/scss/**/*xyz.scss')
-  .pipe(clean());
-});
-
-
-gulp.task('css-init',['rn-layout','rm-layout']);
-gulp.task('css-reset',['rn-xyz','rm-xyz']);
-/// end of layout scss fix
 
 gulp.task('css', function() {
     return gulp
@@ -56,10 +21,8 @@ gulp.task('css', function() {
             .pipe( gulp.dest('./build/css/') );
 });
 
-gulp.task('compile',['css-init','css','css-reset']);
-
 gulp.task('watch', function() {
-  gulp.watch(['assets/scss/**/*.scss'],['compile']);
+  gulp.watch(['assets/scss/**/*.scss'],['css']);
 });
 
 gulp.task('min-css', function(){
