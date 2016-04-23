7722535860b7e8f93f9687138bbcdfb127b96c78
seeliang <si@sis-MacBook.local>
2016-04-23 23:40:23 +1000

    add gulp eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  eslint = require('gulp-eslint');
 require('sl-gt-synclint')(gulp);
+
+//should add exclude at src
+//should use json for setting
+
+gulp.task('eslint', function eslintGl() {
+  return gulp.src(['**/*.js','!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
