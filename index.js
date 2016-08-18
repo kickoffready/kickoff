f12fd3e9f06048b3c11d14779aa58fb662ce4a82
seeliang <si@sis-MacBook.local>
2016-08-19 08:35:21 +1000

    init
    shall fix the
    https://github.com/gaearon/react-hot-loader/issues/68

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// index.js
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
-// import routes and pass them into <Router/>
 import routes from './modules/routes'
+//var routes = require('./modules/routes'); // quick fixc
 
 render(
   <Router routes={routes} history={browserHistory}/>,
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+        test: /\.js$/, exclude: /node_modules/,
+        //loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        loaders: ['react-hot','babel-loader?presets[]=es2015&presets[]=react']
+      }
     ]
   }
 }
