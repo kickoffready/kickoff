8e9b1e1af6efd1116250dd3d3b22ec61567627e6
Sihui liang <me@sihuiliang.com>
2017-04-06 19:12:08 +1000

    hook react with redux

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -3,22 +3,25 @@ import {render} from 'react-dom';
 import reducers from './reducers';
 import {addImage,imagesFetch} from './actions';
 import store from './store';
-import { Provider} from 'react-redux';
 
 class Demo extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.add = () => {
+      store.dispatch(addImage('450x456'));
+    }
+  }
   componentWillMount(){
     store.dispatch(imagesFetch());
-    store.dispatch(addImage('320x240'));
   }
   displayName: 'Demo';
   render(){
     return (
-      <Provider store={store}>
-        <section className={'react-demo'}>
-          <h1>here is my react redux demo</h1>
-          <button>add</button>
-        </section>
-      </Provider>
+      <section className={'react-demo'}>
+        <h1>here is my react redux demo</h1>
+        <button onClick={this.add.bind(this)}>add</button>
+      </section>
     );
   }
 }
