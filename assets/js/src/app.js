8aa831f8b23643339899cdd66533ba328cbd1fe1
seeliang <sihui@sihuis-MacBook-Pro.local>
2017-04-06 17:27:17 +1000

    update with component will mount

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -3,23 +3,22 @@ import {render} from 'react-dom';
 import reducers from './reducers';
 import {addImage,imagesFetch} from './actions';
 import store from './store';
-
-store.subscribe(() => {
-  console.log('new store input', store.getState());
-});
-
-store.dispatch(imagesFetch());
-
-store.dispatch(addImage('320x240'));
+import { Provider} from 'react-redux';
 
 class Demo extends React.Component{
+  componentWillMount(){
+    store.dispatch(imagesFetch());
+    store.dispatch(addImage('320x240'));
+  }
   displayName: 'Demo';
   render(){
     return (
-      <section className={'react-demo'}>
-        <h1>here is my react demo</h1>
-        <h2>something new</h2>
-      </section>
+      <Provider store={store}>
+        <section className={'react-demo'}>
+          <h1>here is my react redux demo</h1>
+          <button>add</button>
+        </section>
+      </Provider>
     );
   }
 }
