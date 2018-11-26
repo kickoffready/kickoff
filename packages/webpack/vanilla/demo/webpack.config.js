5de004e3d16204422b58c1dc643c2ac53a45b9a7
seeliang <me@sihuiliang.com>
2018-11-27 08:22:22 +1100

    chore: add test options

--- packages/webpack/vanilla/demo/webpack.config.js
+++ packages/webpack/vanilla/demo/webpack.config.js
@@ -1,4 +1,17 @@
 const {config} = require('ko-webpack-vanilla');
 const path = require('path');
 
+const options = {
+  entry: {
+    app: './src/index.js',
+  },
+
+  output: {
+    path: ('/new/js'),
+    publicPath: 'new/js',
+    filename: '[name].js',
+  },
+};
+
+
 module.exports = config();
