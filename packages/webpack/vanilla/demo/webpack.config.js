8f2d89efd4e4a318c153868cb79cc3f8567587ee
seeliang <me@sihuiliang.com>
2018-11-27 08:18:12 +1100

    chore: join the set

--- packages/webpack/vanilla/demo/webpack.config.js
+++ packages/webpack/vanilla/demo/webpack.config.js
@@ -1,18 +1,4 @@
-let config;
-const {prod, dev} = require('ko-webpack-vanilla');
+const {config} = require('ko-webpack-vanilla');
 const path = require('path');
 
-if(process.env.NODE_ENV === 'prod') {
-  config = prod();
-}
-
-if(process.env.NODE_ENV === 'dev') {
-  config = dev();
-}
-
-if(typeof(config) !== 'object') {
-  console.log('\n' + 'Oh my, we missing NODE_ENV set, that is bad' + '\n'); 
-  return;
-}
-
-module.exports = config;
+module.exports = config();
