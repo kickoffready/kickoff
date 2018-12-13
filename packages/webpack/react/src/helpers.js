96d3fcc76d76df2240521a9392c67dbed5267050
seeliang <me@sihuiliang.com>
2018-12-13 18:25:27 +1100

    feat: remove default set

--- packages/webpack/react/src/helpers.js
+++ packages/webpack/react/src/helpers.js
@@ -1,13 +1,4 @@
-const defaultOptions = require('./default');
 const path = require('path');
-
-const optionsFallback = options => {
- if (typeof options !== 'object') {
-    console.log('loading default options')
-    return defaultOptions;
-  }
-  return options;
-}
 
 const appPath = (location) => path.resolve(__dirname + '../../../' + location);
 const absoluteEntry = entry => Object.keys(entry).reduce((r, i) => ({ ...r, [i]: appPath(entry[i])}), {});
