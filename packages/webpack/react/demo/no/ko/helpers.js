064d6c88079d5caea98edcdb9952e3d1db6c7f5c
seeliang <me@sihuiliang.com>
2018-12-30 17:52:13 +1100

    chore: move test code to correct folder level

--- packages/webpack/react/demo/no/ko/helpers.js
+++ packages/webpack/react/demo/no/ko/helpers.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const appPath = (location) => path.resolve(__dirname + '../../../../' + location);
+const appPath = (location) => path.resolve(__dirname + '../../../' + location);
 const absoluteEntry = entry => Object.keys(entry).reduce((r, i) => ({ ...r, [i]: appPath(entry[i])}), {});
 
 
