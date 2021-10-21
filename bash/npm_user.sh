9e4539619a3da19a0f965875281c82f496fc4892
seeliang <hi@sihuiliang.com>
2021-10-21 15:02:57 +1100

    chore: remove unused

--- bash/npm_user.sh
+++
@@ -1,4 +0,0 @@
-echo "//registry.npmjs.org/:_password:YOUR_NPM_PASSWORD" >> .npmrc
-echo "//registry.npmjs.org/:username:seeliang" >> .npmrc
-echo "//registry.npmjs.org/:email:hi@sihuiliang.com" >> .npmrc
-echo "//registry.npmjs.org/:always-auth=false" >> .npmrc
