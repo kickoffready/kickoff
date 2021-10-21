d81c65fe9a6933ae38220bf24c16aa1053ade327
seeliang <hi@sihuiliang.com>
2021-10-21 14:29:10 +1100

    chore: may need bash

---
+++ bash/npm_user.sh
@@ -0,0 +1,4 @@
+echo "//registry.npmjs.org/:_password:YOUR_NPM_PASSWORD" >> .npmrc
+echo "//registry.npmjs.org/:username:seeliang" >> .npmrc
+echo "//registry.npmjs.org/:email:hi@sihuiliang.com" >> .npmrc
+echo "//registry.npmjs.org/:always-auth=false" >> .npmrc
