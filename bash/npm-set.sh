a61ef18d7198ae6ae952bb2dd9b5a4b7ab96303e
seeliang <hi@sihuiliang.com>
2021-10-25 10:16:11 +1100

    chore: add org to reg

--- bash/npm-set.sh
+++ bash/npm-set.sh
@@ -2,3 +2,4 @@ npm set //registry.npmjs.org/:username seeliang
 npm set //registry.npmjs.org/:email hi@sihuiliang.com
 npm set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
 npm set registry https://registry.npmjs.org/
+npm set @kickoffready:registry https://registry.npmjs.org/
