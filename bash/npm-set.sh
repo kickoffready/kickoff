95d9947b2f6eee356649f8fe7984caa8ace90c97
seeliang <hi@sihuiliang.com>
2021-10-25 10:55:06 +1100

    chore: try auth true

--- bash/npm-set.sh
+++ bash/npm-set.sh
@@ -3,3 +3,4 @@ npm set //registry.npmjs.org/:email hi@sihuiliang.com
 npm set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
 npm set registry https://registry.npmjs.org/
 npm set @kickoffready:registry https://registry.npmjs.org/
+npm set always-auth true
