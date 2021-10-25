cd1347d1dcb78da00e86d663599710c052f17dd3
seeliang <hi@sihuiliang.com>
2021-10-25 16:16:00 +1100

    chore: fix token set

--- bash/npm-set.sh
+++ bash/npm-set.sh
@@ -1,4 +1,4 @@
 npm set //registry.npmjs.org/:username seeliang
 npm set email hi@sihuiliang.com
-npm set _auth ${NPM_TOKEN}
+npm set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
 npm set registry https://registry.npmjs.org/
