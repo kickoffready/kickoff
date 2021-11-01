a759fa42ed099fbc6152649ed2bcada30301fade
seeliang <hi@sihuiliang.com>
2021-11-02 10:32:17 +1100

    feat: add ts base rules

--- packages/lint/lib/eslint/rules.js
+++ packages/lint/lib/eslint/rules.js
@@ -7,6 +7,14 @@ const react = {
     },
   },
 };
+const ts = {
+  rules: {
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': 2,
+  },
+};
 
 module.exports = {
   react,
